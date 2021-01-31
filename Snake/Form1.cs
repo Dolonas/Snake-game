431586124d8b4e7c143a4035c058045550c238db
Dolonas <pavelcoin@gmail.com>
2021-01-31 23:38:50 +0400

    Level has been added to game

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -21,6 +21,7 @@ namespace Snake
         private Panel PanelGameField;  // элемент в которое будет помещено игровое поле
         private Panel PanelMng;  // элемент формы, на котором будут располагаться органы управления
         private Label labelScore;   //  текстовый элемент с подсчётом очков
+        private Label labelLevel;   //  текстовый элемент с подсчётом очков
         private GroupBox GBoxMng;   //  текстовый элемент с подсчётом очков
         private MyButton buttonPause;
         private MyButton buttonExit;
@@ -42,6 +43,7 @@ namespace Snake
         private int dirX = 1;
         private int dirY = 0;
         private int score = 0;
+        private int level = 1;
         private bool pauseFlag = false;
         private WindowsMediaPlayer WMP;
         private string melodyFile;
@@ -95,15 +97,13 @@ namespace Snake
 
             PanelMng = new Panel();
             PanelMng.ForeColor = Color.White;
-            PanelMng.BackColor = Color.FromArgb(40,40,40);
+            PanelMng.BackColor = Color.FromArgb(65, 65, 65);
             PanelMng.Location = new Point(xPanelSize+2, 2);
             PanelMng.Size = new Size(this.Size.Width - xPanelSize - 2, yPanelSize);
             this.Controls.Add(PanelMng);
 
             GBoxMng = new GroupBox();
             GBoxMng.Text = "";
-            //GBoxMng.BackColor = Color.FromArgb(50, 50, 50);
-            //GBoxMng.ForeColor = Color.FromArgb(40, 40, 40);
             GBoxMng.FlatStyle = FlatStyle.Flat;
             GBoxMng.Location = new Point(5, 0);
             GBoxMng.Size = new Size(PanelMng.Width-25, yPanelSize-5);
@@ -113,12 +113,19 @@ namespace Snake
 
             labelScore = new Label();
             labelScore.Font = new Font(labelScore.Font.FontFamily, 12, labelScore.Font.Style);
-            labelScore.Anchor = AnchorStyles.Top;
             labelScore.ForeColor = Color.White;
             labelScore.Text = "Очки: 0";
             labelScore.Size = new Size(80, 40);
             labelScore.Location = new Point(22, 30);
             GBoxMng.Controls.Add(labelScore);
+
+            labelLevel = new Label();
+            labelLevel.Font = new Font(labelScore.Font.FontFamily, 12, labelScore.Font.Style);
+            labelLevel.ForeColor = Color.White;
+            labelLevel.Text = "Уровень: 1";
+            labelLevel.Size = new Size(100, 40);
+            labelLevel.Location = new Point(12, 80);
+            GBoxMng.Controls.Add(labelLevel);
 
             buttonPause = new MyButton();
             buttonPause.Location = new Point(12, 200);
@@ -140,7 +147,7 @@ namespace Snake
             snake[0] = new GameItem();
             snake[0].XCoor = 0;
             snake[0].YCoor = 0;
-            snake[0].BackColor = Color.FromArgb(214,66,255);
+            snake[0].BackColor = Color.FromArgb(41,142,25);
             PanelGameField.Controls.Add(snake[0]);
         }
 
@@ -186,10 +193,11 @@ namespace Snake
                 ChangeScore(++score);
                 snake[score] = new GameItem();
                 snake[score].LocationField = new Point(snake[score - 1].LocationField.X + dirX, snake[score - 1].LocationField.Y + dirY);
-                snake[score].BackColor = Color.FromArgb(214, 66, 255);
+                snake[score].BackColor = Color.FromArgb(73, 255, 45);
                 PanelGameField.Controls.Add(snake[score]);
                 GenerateFruit();
             }
+            ChangeLevel();
         }
 
         private void FruitInit()
@@ -379,6 +387,60 @@ namespace Snake
         private void ChangeScore(int amount)
         {
             labelScore.Text = "Очки: " + amount;
+        }
+
+        private void ChangeLevel()
+        {
+            if (score < 10)
+                level = 1;
+            else if (score < 20)
+                level = 2;
+            else if (score < 30)
+                level = 3;
+            else if (score < 40)
+                level = 4;
+            else if (score < 50)
+                level = 5;
+            else if (score < 60)
+                level = 7;
+            else if (score < 70)
+                level = 7;
+            else if (score < 80)
+                level = 8;
+            else if (score < 90)
+                level = 9;
+            else if (score < 100)
+                level = 10;
+            else if (score > 99)
+                YouWon();
+
+            labelLevel.Text = "Уровень: " + level.ToString();
+            timer.Interval = 300 - 20 * level;
+        }
+
+        private void YouWon()
+        {
+            MakePause(null, null);
+            var result = MessageBox.Show("Вы победили", "Ура!", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+            if (result == DialogResult.OK)
+            {
+                AgainQuestion();
+            }
+           
+        }
+
+        private void AgainQuestion()
+        {
+            var result = MessageBox.Show("Сыграем ещё раз?", "Может быть", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.OK)
+            {
+                this.Close();
+            }
+            else if (pauseFlag)
+            {
+                this.Close();
+            }
         }
     }
 
